f55e75e3590d68c913775e3319f11db33df4133d
Angelina Cortes <cortesporcz@gmail.com>
2024-04-17 10:44:29 -0500

    creating files for heading functions

---
+++ src/components/HeroImg2.js
@@ -0,0 +1,15 @@
+import "./HeroImg2Styles.css"
+
+import React from 'react'
+
+const HeroImg2 = () => {
+  return (
+    <div className="hero-img">
+        <div className="heading">
+            
+        </div>
+    </div>
+  )
+}
+
+export default HeroImg2
