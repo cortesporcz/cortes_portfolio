694e4e708107c584bdddb1720868ecc24e749146
Angelina Cortes <cortesporcz@gmail.com>
2024-04-17 09:41:00 -0500

    adding style to home page and background (working on btns)

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -6,15 +6,15 @@ import { Link } from "react-router-dom";
 
 const HeroImg = () => {
   return <div className="hero">
-        <div className="mask">
+         <div className="mask">
            <img className="intro-img" src={IntroImg} alt="IntroImg" /> 
         </div>  
         <div className="content">
             <p>Hi, I'm Angelina M. Cortes</p>
             <h1>Developer and Librarian</h1>
         </div>
-        <Link to="/project" className='btn'>Projects</Link>
-        <Link to="/contact" className='btn btn-light'>Contact</Link>
+        <Link to="/project" className="btn">Projects</Link>
+        <Link to="/contact" className="btn btn-light">Contact</Link>
 
     </div> ; 
 }
