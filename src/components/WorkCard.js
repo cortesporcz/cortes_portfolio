93fa26f1f78ee4916e81c9c40d16af6c3a3b906a
Angelina Cortes <cortesporcz@gmail.com>
2024-04-18 15:02:47 -0500

    created word card data for boilerplate js

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -5,21 +5,16 @@ import { NavLink } from "react-router-dom";
 
 const WorkCard = () => {
   return (
-    <div className="work-container">
-        <h1 className="project-heading">Projects</h1>
-        <div className="project-container">
-            <div className="project-card">
-                <image src={pro1} alt="image" />
-                <h2 className="project-title">Project Title</h2>
-                <div className="pro-details">This is a p place holder</div>
-                <p>Add text here</p>
-                <div className="pro-btns">
-                   <NavLink to="url.com" className="btn">View</NavLink>
-                    <NavLink to="btn">Source</NavLink>
-                </div>
-            </div>
-        </div>
+    <div className="project-card">
+    <image src={pro1} alt="image" />
+    <h2 className="project-title">Project Title</h2>
+    <div className="pro-details">This is a p place holder</div>
+    <p>Add text here</p>
+    <div className="pro-btns">
+       <NavLink to="url.com" className="btn">View</NavLink>
+        <NavLink to="btn">Source</NavLink>
     </div>
+</div>
   );
 }
 
