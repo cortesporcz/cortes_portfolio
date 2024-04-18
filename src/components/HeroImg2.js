79fa91d9fee465dbb38d082085200faa284aec55
Angelina Cortes <cortesporcz@gmail.com>
2024-04-18 08:55:55 -0500

    adding portfolio style and background

--- src/components/HeroImg2.js
+++ src/components/HeroImg2.js
@@ -6,7 +6,9 @@ const HeroImg2 = () => {
   return (
     <div className="hero-img">
         <div className="heading">
-            
+          <h1>This is a heading</h1>
+            <p>This is a text</p>
+                
         </div>
     </div>
   )
