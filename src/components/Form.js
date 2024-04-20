dc4766031b5f208ba248d87e16c9e92f6c385f40
Angelina Cortes <cortesporcz@gmail.com>
2024-04-20 11:18:44 -0500

    adding styling to contact form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,20 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <div className="form">
+        <form>
+            <label>Your Name</label>
+            <input type="text"></input>
+            <label>Email</label>
+            <input type="text"></input>
+            <label>Subject</label>
+            <input type="text"></input>
+            <label>Message</label>
+            <input type="text"></input>
+            <textarea rows="6" placeholder="Type Your Message here" />
+            <button className="btn">Submit</button>
+        </form>
+    </div>
   )
 }
 
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,14 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import HeroImg2 from "../components/HeroImg2";
 import Footer from "../components/Footer";
-
-
+import Form from "../components/Form";
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
       <HeroImg2 heading="CONTACT" text="Let's chat!"/>
+      <Form />
       <Footer />
     </div>
   )
