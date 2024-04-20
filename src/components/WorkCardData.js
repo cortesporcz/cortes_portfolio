693afaa36e3f75541bddf1156138098284cb3286
Angelina Cortes <cortesporcz@gmail.com>
2024-04-20 10:16:58 -0500

    adding data for work cards

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,28 @@
+import pro1 from "../assets/project1.jpg";
+import pro2 from "../assets/project2.jpg";
+import pro3 from "../assets/project3.jpg";
+
+const ProjectCardData = [{
+imgsrc: pro1,
+title: "Eccommerce Website Design",
+text: "Placeholder for descriptive text",
+view: "add link here"
+},
+
+{
+    imgsrc: pro2,
+    title: "Eccommerce Website Design",
+    text: "Placeholder for descriptive text",
+    view: "add link here"
+},
+
+{
+    imgsrc: pro3,
+    title: "Eccommerce Website Design",
+    text: "Placeholder for descriptive text",
+    view: "add link here"
+}
+]; 
+
+export default ProjectCardData; 
+
